654c8df3535010db7f4956be3604a1658398b127
z(0o0a) <i7can7i@gmail.com>
2023-03-08 02:06:22 +0900

    호스트는 나가있어 앱 디지기 싫으면

--- app/src/main/java/com/z0o0a/navigationtest/view/MainActivity.kt
+++ app/src/main/java/com/z0o0a/navigationtest/view/MainActivity.kt
@@ -3,15 +3,11 @@ package com.z0o0a.navigationtest.view
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.z0o0a.navigationtest.R
-import com.z0o0a.navigationtest.databinding.ActivityMainBinding
 
-class MainActivity : AppCompatActivity() {
-    private lateinit var binding: ActivityMainBinding
+class MainActivity : AppCompatActivity(R.layout.activity_main) {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        binding = ActivityMainBinding.inflate(layoutInflater)
-        val view = binding.root
-        setContentView(view)
+
     }
 }
